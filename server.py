9b207ed30a4de94a06c9023ba236f55d30278fd5
SDD3108 <mrbimson1@gmail.com>
2026-02-16 19:18:39 +0500

    First commit

---
+++ server.py
@@ -0,0 +1,21 @@
+# matrix = [
+#     [1,2,3],
+#     [4,5,6],
+#     [7,8,9],
+# ]
+
+# sum = 0
+
+# for i in matrix:
+#     for j in i:
+#         sum += j
+
+# print(sum)
+
+# for j in range(3):
+#     col_sum = 0
+#     for i in matrix:
+#         col_sum += i[j]
+#     print(col_sum)
+
+
